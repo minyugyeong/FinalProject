46deb54eaa9540ac974be942ac8f40b178a4a39b
KimDongSun7 <rla9960@naver.com>
2022-06-27 20:23:34 +0900

    CertDto 생성

---
+++ com.kh.mars/src/main/java/com/kh/mars/entity/CertDto.java
@@ -0,0 +1,16 @@
+package com.kh.mars.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class CertDto {
+
+	private String certTarget;
+	private String certNumber;
+	private Date cerTime;
+}
