bfed633e66146bd1de94e0df33b1fe2108764189
minyugyeong <alsdbrud123@naver.com>
2022-06-27 15:08:15 +0900

    Dto 생성 - BoardAttachDto - BoardAdCheckDto - BoardAdReplyDto - BoardAttachDto - BoardHashtagDto - BoardLikeDto

---
+++ com.kh.mars/src/main/java/com/kh/mars/entity/BoardAdAttachDto.java
@@ -0,0 +1,14 @@
+package com.kh.mars.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class BoardAdAttachDto {
+	
+	private int boardAdAttachNo;
+	private int boardAdNo;
+	private int attachNo;
+}
---
+++ com.kh.mars/src/main/java/com/kh/mars/entity/BoardAdReplyDto.java
@@ -0,0 +1,13 @@
+package com.kh.mars.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class BoardAdReplyDto {
+	
+	private int replyNo;
+	private int boardAdNo;
+}
