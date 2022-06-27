06844f797cc03b75653232646910a651c85803be
chlqjatlr1 <eorlsha@naver.com>
2022-06-27 17:51:34 +0900

    세팅중

---
+++ com.kh.mars/src/main/java/com/kh/mars/vo/MessageVO.java
@@ -0,0 +1,15 @@
+package com.kh.mars.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
+public class MessageVO {
+	private String memberId;
+	private String text;
+	private String memberAuth;
+	private Date time;
+}
