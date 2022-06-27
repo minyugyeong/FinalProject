03e914fb9d3f40e92c550c761fc8f49d6edf6d09
chlqjatlr1 <eorlsha@naver.com>
2022-06-27 15:34:43 +0900

    dto작성

---
+++ com.kh.mars/src/main/java/com/kh/mars/entity/DmDto.java
@@ -0,0 +1,12 @@
+package com.kh.mars.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor@Builder
+public class DmDto {
+	private int dmNo;
+	private String dmContent;
+}
