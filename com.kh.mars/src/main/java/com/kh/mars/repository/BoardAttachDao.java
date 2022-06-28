230248c9e4dcdd86fd8c59d6bdd6b2a79eb65485
minyugyeong <alsdbrud123@naver.com>
2022-06-28 09:59:33 +0900

    첨부파일 구현 중....

--- com.kh.mars/src/main/java/com/kh/mars/repository/BoardAttachDao.java
+++ com.kh.mars/src/main/java/com/kh/mars/repository/BoardAttachDao.java
@@ -1,5 +1,9 @@
 package com.kh.mars.repository;
 
+import com.kh.mars.entity.BoardAttachDto;
+
 public interface BoardAttachDao {
+	
+	void insert(BoardAttachDto boardAttachDto);
 
 }
--- com.kh.mars/src/main/java/com/kh/mars/repository/BoardAttachDaoImpl.java
+++ com.kh.mars/src/main/java/com/kh/mars/repository/BoardAttachDaoImpl.java
@@ -4,10 +4,23 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.mars.entity.BoardAttachDto;
+
 @Repository
 public class BoardAttachDaoImpl implements BoardAttachDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
+
+	
+	@Override
+	public void insert(BoardAttachDto boardAttachDto) {
+		int sequence = sqlSession.selectOne("board_attach.sequence");
+		boardAttachDto.setBoardAttachNo(sequence);
+		
+		sqlSession.insert("board_attach.insert", boardAttachDto);
+		
+	}
+
 
 }
